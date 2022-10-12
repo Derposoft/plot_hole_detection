c99e89dd23ba98facb51ae828193c52eee13dde1
derposoft <viswanath.chadalapaka@gmail.com>
2022-10-11 19:54:22 -0700

    +working generation for all synthetic data

--- data/generate_synthetic_data.py
+++ data/generate_synthetic_data.py
@@ -3,6 +3,7 @@ from typing import List, Tuple
 import numpy as np
 from copy import deepcopy
 #from negation_conversion import applyNegation
+ROOT = Path(__file__)
 
 
 def is_txt(path) -> bool:
@@ -11,9 +12,8 @@ def is_txt(path) -> bool:
 
 def get_datafiles() -> list:
     """
-    Returns list of stories(.txt file) in raw_story_file folder
-    """    
-    ROOT = Path(__file__)
+    returns list of stories(.txt file) in raw_story_file folder
+    """
     return [x for x in Path(ROOT.parent / "raw").iterdir() if is_txt(x)]
 
 
@@ -31,15 +31,16 @@ def generate_continuity_errors(document: str, n: int) -> Tuple[List[str], List[i
     for sample in samples:
         X.append(deepcopy(sentences))
         #X[-1][sample] = "".join([word if word not in negate or not word.endswith("ed") else word + " not " for word in X[-1][sample]])
-        synthetic_document = []
+        synthetic_sentence = []
         for word in X[-1][sample].split(" "):
             if word.endswith("ed"):
-                synthetic_document.append("was not "+word)
+                synthetic_sentence.append("was not "+word)
             elif word in negate:
-                synthetic_document.append(word + " not")
+                synthetic_sentence.append(word + " not")
             else:
-                synthetic_document.append(word)
-        X[-1][sample] = " ".join(synthetic_document)
+                synthetic_sentence.append(word)
+        X[-1][sample] = " ".join(synthetic_sentence)
+    X = [" ".join(x) for x in X]
     y = samples
     return X, y
 
@@ -66,13 +67,38 @@ def generate_unresolvedstory_errors(document: str, n: int, p:float=0.1) -> Tuple
     return X, y
 
 
+def write_synthetic_datapoint_to_file(X, y, path, plot_hole_type):
+    """
+    write a synthetic datapoint to a file.
+    :param X: synthetic document
+    :param y: synthetic label
+    :param path: path to write the file to
+    :param plot_hole_type: type of plot hole, will be written at top of document
+    :returns: None. file will be written at path. first line will be "plot_hole_type y", and
+    rest of the lines will be X.
+    """
+    with open(path, "w") as synthetic_document_f:
+        synthetic_document_f.write(f"{plot_hole_type} {y}\n")
+        synthetic_document_f.write(X)
+
+
 if __name__ == "__main__":
     n = 10
     dataset = get_datafiles()
-    for document in dataset:
-        with open(document, "r", encoding="utf8") as document:
-            document = " ".join([x.strip() for x in document.readlines()])
+    for doc_path in dataset:
+        with open(doc_path, "r", encoding="utf8") as document_f:
+            document = " ".join([x.strip() for x in document_f.readlines()])
             X_continuity, y_continuity = generate_continuity_errors(document, n)
             X_unresolved, y_unresolved = generate_unresolvedstory_errors(document, n)
-        print(X_continuity[0][y_continuity[0]], y_continuity[0])
-        print(X_unresolved[0], y_unresolved[0])
+            for i in range(n):
+                doc_name = str(doc_path).split("/")[-1].split(".")[0]
+                continuity_path = f"data/synthetic/synthetic_{doc_name}_continuity{i}.txt"
+                unresolved_path = f"data/synthetic/synthetic_{doc_name}_unresolved{i}.txt"
+                X, y = X_continuity[i], y_continuity[i]
+                write_synthetic_datapoint_to_file(
+                    X=X, y=y, path=continuity_path, plot_hole_type="continuity"
+                )
+                X, y = X_unresolved[i], y_unresolved[i]
+                write_synthetic_datapoint_to_file(
+                    X=X, y=y, path=unresolved_path, plot_hole_type="unresolved"
+                )
