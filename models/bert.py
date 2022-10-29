d2993fe7f46f05c6ffdf05ecff87a3ea2debdb77
derposoft <viswanath.chadalapaka@gmail.com>
2022-10-29 15:35:40 -0700

    +working baselines

--- models/bert.py
+++ models/bert.py
@@ -18,18 +18,21 @@ class ContinuityBERT(nn.Module):
         # encoder encodes input sentences
         self.encoder, self.encoder_dim = create_sentence_encoder(encoder_name)
         # decider decides which sentences are continuity errors
-        self.decider = nn.Transformer(nhead=n_heads, d_model=self.encoder_dim)
-        # softmax normalizes decider outputs
-        self.softmax = nn.Softmax()
+        self.decider = nn.Transformer(nhead=n_heads, d_model=self.encoder_dim, batch_first=True)
+        # project feature space to single probability
+        self.proj = nn.Linear(self.encoder_dim, 1)
+        # softmax normalizes all proj outputs to find sentence
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x: List[List[str]]):
         """
         :param x: sequence of sentences from a story with shape batch_size, n_sentences, 
         :returns: sequence of logits for each sentence
         """
-        x = self.encoder.encode(x)
-        print("sentence encodings shape", x.shape)
+        x = torch.stack([torch.Tensor(self.encoder.encode(_x)) for _x in x])
         x = self.decider(x, torch.zeros(x.shape))
+        x = self.proj(x)
+        x = x.reshape([x.shape[0], -1])
         return self.softmax(x)
 
 
@@ -39,11 +42,23 @@ class UnresolvedBERT(nn.Module):
     i.e., whether or not the story was cut short before the storyline 
     was resolved.
     """
-    def __init__(self):
+    def __init__(self, n_heads=16, encoder_name="all-MiniLM-L6-v2"):
         nn.Module.__init__(self)
-        pass
+        # encoder encodes input sentences
+        self.encoder, self.encoder_dim = create_sentence_encoder(encoder_name)
+        # decider decides which sentences are most important in deciding how "incomplete" story is
+        self.decider = nn.Transformer(nhead=n_heads, d_model=self.encoder_dim, batch_first=True)
+        # project feature space to single probability
+        self.proj = nn.Linear(self.encoder_dim, 1)
+        # sigmoid function to determine percentage of story cut off
+        self.sigmoid = nn.Sigmoid()
+        
     def forward(self, x):
-        pass
+        x = torch.stack([torch.Tensor(self.encoder.encode(_x)) for _x in x])
+        x = self.decider(x, torch.zeros([x.shape[0], 1, x.shape[-1]]))
+        x = self.proj(x)
+        x = x.reshape([x.shape[0], -1])
+        return self.sigmoid(x)
 
 
 def create_sentence_encoder(encoder_name="all-MiniLM-L6-v2"):
@@ -64,12 +79,32 @@ def create_sentence_encoder(encoder_name="all-MiniLM-L6-v2"):
 
 
 if __name__ == "__main__":
+    batch_size = 2
+    
+    """
+    test ContinuityBERT model
+    """
     with open("data/synthetic/synthetic_text1_continuity0.txt", "r") as f:
         lines = f.readlines()
+    # model output
     continuity_model = ContinuityBERT()
-    tgt_idx = int(lines[0].split()[1])
-    y = torch.zeros(len(lines)-1)
-    y[tgt_idx] = 1
-    x = [lines[1:], lines[1:]]
+    x = [lines[1:]] * batch_size
     y_hat = continuity_model(x)
-    print(y_hat.shape, y.shape)
+    # expected output
+    tgt_idx = int(lines[0].split()[1])
+    y = torch.zeros((batch_size, len(lines)-1))
+    y[:,tgt_idx] = 1
+    print(f"output shape: {y_hat.shape}, expected shape: {y.shape}")
+
+    """
+    test UnresolvedBERT model
+    """
+    with open("data/synthetic/synthetic_text1_unresolved0.txt", "r") as f:
+        lines = f.readlines()
+    # model output
+    unresolved_model = UnresolvedBERT()
+    x = [lines[1:]] * batch_size
+    y_hat = unresolved_model(x)
+    # expected output
+    y = torch.zeros((batch_size, 1))
+    print(f"output shape: {y_hat.shape}, expected shape: {y.shape}")
