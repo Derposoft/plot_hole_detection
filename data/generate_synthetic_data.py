8bff2edf8b1b268c2081ef288bf2f8d5ae80ecbb
derposoft <viswanath.chadalapaka@gmail.com>
2022-10-30 21:09:31 -0700

    +train test split implemented

--- data/generate_synthetic_data.py
+++ data/generate_synthetic_data.py
@@ -112,17 +112,20 @@ def write_synthetic_datapoint_to_file(X, y, path, plot_hole_type):
         synthetic_document_f.write(X[1:])
 
 
-def generate_synthetic_data(n=10):
+def generate_synthetic_data(n=10, train_ratio=0.8):
     dataset = get_datafiles()
-    for doc_path in dataset:
+    n_docs = len(dataset)
+    for doc_idx in range(len(dataset)):
+        train_test_prefix = "train/" if doc_idx < n_docs*train_ratio else "test/"
+        doc_path = dataset[doc_idx]
         with open(doc_path, "r", encoding="utf8") as document_f:
             document = " ".join([x.strip() for x in document_f.readlines()])
             X_continuity, y_continuity = generate_continuity_errors(document, n)
             X_unresolved, y_unresolved = generate_unresolvedstory_errors(document, n)
             for i in range(n):
                 doc_name = str(doc_path).split("\\" if platform=="win32" else "/")[-1].split(".")[0]
-                continuity_path = ROOT.parent / f"synthetic/synthetic_{doc_name}_continuity{i}.txt"
-                unresolved_path = ROOT.parent / f"synthetic/synthetic_{doc_name}_unresolved{i}.txt"
+                continuity_path = ROOT.parent / f"synthetic/{train_test_prefix}synthetic_{doc_name}_continuity{i}.txt"
+                unresolved_path = ROOT.parent / f"synthetic/{train_test_prefix}synthetic_{doc_name}_unresolved{i}.txt"
                 X, y = X_continuity[i], y_continuity[i]
                 write_synthetic_datapoint_to_file(
                     X=X, y=y, path=continuity_path, plot_hole_type="continuity"
--- train.py
+++ train.py
@@ -5,17 +5,42 @@ import torch.nn as nn
 from torch.optim import Adam
 
 
-def train(*, model, data, opt, criterion, epochs=10, verbose=True):
+def test(*, model, test_data, metrics="f1", verbose=True):
+    """
+    :param model: the model to test
+    :param test_data: test dataloader
+    :param metrics: one of either "f1" or "mse".
+    :param verbose: whether or not to print extra output
+    """
+    y_preds = []
+    y_true = []
+    for _, (X, y) in enumerate(test_data):
+        y_preds.append(model(X))
+        y_true.append(y)
+    # TODO calculate metrics here using y_preds and y_true. find f1 score if metrics="f1", otherwise calculate mse.
+    results = "TODO"
+    # TODO print metrics here
+    print(f"{metrics} score: {results}")
+
+
+def train(*, model, train_data, test_data, opt, criterion, epochs=10, metrics="f1", verbose=True):
+    """
+    :param model: the model to test
+    :param train_data: train dataloader
+    :param test_data: test dataloader
+    :param verbose: whether or not to print extra output
+    """
     for epoch in range(epochs):
         if verbose:
             print(f"starting epoch {epoch}")
-        for i, (X, y) in enumerate(data):
+        for i, (X, y) in enumerate(train_data):
             y_hat = model(X)
             loss = criterion(y_hat, y)
             loss.backward()
             opt.step()
             if verbose and i%1==0:
-                print(loss)
+                print(f"batch {i} loss: {loss.item()}")
+        test(model=model, test_data=test_data, metrics=metrics, verbose=verbose)
 
 
 def parse_args():
@@ -50,7 +75,12 @@ if __name__ == "__main__":
 
     # read data
     print("reading data...")
-    continuity_dataloader, unresolved_dataloader = utils.read_data(batch_size=batch_size, n_stories=n_stories)
+    continuity_train, unresolved_train = utils.read_data(
+        batch_size=batch_size, n_stories=n_stories, data_path="data/synthetic/train", cache_path="data/encoded/train"
+    )
+    continuity_test, unresolved_test = utils.read_data(
+        batch_size=batch_size, n_stories=n_stories, data_path="data/synthetic/test", cache_path="data/encoded/test"
+    )
     print("done.")
 
     # train continuity model
@@ -59,7 +89,8 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
     train(
         model=baseline_continuity,
-        data=continuity_dataloader,
+        train_data=continuity_train,
+        test_data=continuity_test,
         opt=opt,
         criterion=criterion,
     )
@@ -71,7 +102,8 @@ if __name__ == "__main__":
     criterion = nn.MSELoss()
     train(
         model=baseline_unresolved,
-        data=unresolved_dataloader,
+        train_data=unresolved_train,
+        test_data=unresolved_test,
         opt=opt,
         criterion=criterion,
     )
